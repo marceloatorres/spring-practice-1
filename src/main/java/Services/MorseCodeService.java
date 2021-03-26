7fec28e558390d12c50384c746d9fea5b9724b9e
Marcelo Torres <marcelo.torres@mercadolibre.com>
2021-03-26 16:18:28 -0300

    fix blank space  separator

--- src/main/java/Services/MorseCodeService.java
+++ src/main/java/Services/MorseCodeService.java
@@ -5,13 +5,13 @@ public class MorseCodeService {
     private static final char[] english = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
             'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x',
             'y', 'z', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0',
-            ',', '.', '?'};
+            ',', '.', '?',' '};
 
     private static final String[] morse = {".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..",
             ".---", "-.-", ".-..", "--", "-.", "---", ".---.", "--.-", ".-.",
             "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--..", ".----",
             "..---", "...--", "....-", ".....", "-....", "--...", "---..", "----.",
-            "-----", "--..--", ".-.-.-", "..--.."};
+            "-----", "--..--", ".-.-.-", "..--..", "   "};
 
     public static String convertToMorse(String words) {
         char[] chars = words.toCharArray();
@@ -21,7 +21,10 @@ public class MorseCodeService {
             for (int j = 0; j < english.length; j++) {
 
                 if (english[j] == chars[i]) {
-                    str = str + morse[j] + " ";
+                    if(str != "")
+                        str += " ";
+
+                    str = str + morse[j];
                 }
             }
         }
